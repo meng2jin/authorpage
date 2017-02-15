864aa3fa97685d20ace94469a74c2c91f452e448
Ge Jin <Gracie@Ges-MacBook-Air.home>
2017-02-15 13:27:10 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var insertContent = function(content, cb) {
+  $('.content-pane').html($(content));
+  $('.content-pane div').fadeIn('1000')
+  if ( typeof cb === 'function' ) {
+    cb();
+  }
+};
+
+var highlightCurrentNavItem = function(route) {
+  var classSelector ='.' + route + '-link';
+  $('.nav-items').find('.active-nav-item').removeClass('active-nav-item');
+  $(classSelector).addClass('active-nav-item');
+};
+
+var homeTypeEffect = function() {
+  var stringToType = 'Meng Jin<br>writes sentences<br>in<br><p class="typed-place">England.</p>';
+  $('.type-container').typed({
+    strings: [stringToType],
+    showCursor: false,
+    typeSpeed: 15,
+    startDelay: 100
+  })
+};
+
+var homeContent = $('#home-content').html();
+var bioContent = $('#bio-content').html();
+var writingContent = $('#writing-content').html();
+var contactContent = $('#contact-content').html();
+
+var routes = {
+  '/': function() {
+    insertContent(homeContent, homeTypeEffect);
+    highlightCurrentNavItem('home');
+  },
+  '/bio': function() {
+    insertContent(bioContent);
+    highlightCurrentNavItem('bio');
+  },
+  '/writing': function() {
+    insertContent(writingContent);
+    highlightCurrentNavItem('writing');
+  },
+  '/contact': function() {
+    insertContent(contactContent);
+    highlightCurrentNavItem('contact');
+  }
+};
+
+var router = Router(routes);
+
+$(function() {
+  router.init('/');
+});
