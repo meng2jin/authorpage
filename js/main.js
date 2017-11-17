844a8e1a0637fa49d8e03f3de8b1aaae0e7e16b2
Neel Patel <neelp22@gmail.com>
2017-11-17 00:16:27 -0500

    Update England to SF
    **on homepage**

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var highlightCurrentNavItem = function(route) {
 };
 
 var homeTypeEffect = function() {
-  var stringToType = 'Meng Jin<br>writes sentences<br>in<br><p class="typed-place">England.</p>';
+  var stringToType = 'Meng Jin<br>writes sentences<br>in<br><p class="typed-place">San Francisco.</p>';
   $('.type-container').typed({
     strings: [stringToType],
     showCursor: false,
